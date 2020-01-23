a03e9bbe5a59f93ed01f214d27931e5404e209ca
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-23 01:52:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
 "I love you too pumpkin".upcase
 if phrase == "Hi Nana, how are you?"
 "HUH?! SPEAK UP, SONNY!"
-else phrase== 
+
 elsif phrase == phrase.upcase
  "NO, NOT SINCE 1938!"
  
